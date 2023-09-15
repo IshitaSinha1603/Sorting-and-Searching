f358a713f70fb86cc14abb142e1b7217324d91b6
ishita <ishitasinha16@gmail.com>
2023-09-15 22:02:43 +0530

    Adding insertion sort

---
+++ Searching/Insertion Sort/insertion.java
@@ -0,0 +1,27 @@
+class insertion {
+    public static void printArray(int[] arr) {
+        for(int i=0;i<arr.length-1;i++) {
+            System.out.println(arr[i]+" ");
+        }
+    }
+    public static void sort(int[] arr) {
+        for(int i=1;i<arr.length-1;i++) {
+            int current=arr[i];
+            int j=i-1;
+            while(j>=0 && current<arr[j]) {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+    }
+    public static void main(String args[]) {
+        int[] arr={7,1,8,0,3,2};
+        System.out.println("Array before sorting:\n");
+        printArray(arr);
+        System.out.println("\n");
+        sort(arr);
+        System.out.println("Array after sorting:\n");
+        printArray(arr);
+    }
+}
